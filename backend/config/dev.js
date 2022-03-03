e8995a7a673280d1241cd8feedfc806b1a79d46e
naamaarkin <87777755+naamaark@users.noreply.github.com>
2022-03-03 14:20:21 +0200

    Delete backend/config directory

--- backend/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
- 'dbURL' :'mongodb+srv://naamaark:GfXdz.4pZ9PgNND@cluster0.gl4ew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- backend/config/prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'dbURL' :'mongodb+srv://naamaark:GfXdz.4pZ9PgNND@cluster0.mkw9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
- }
